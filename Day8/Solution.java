7da659c9e38ee1d89d3be4483006faf7e7428083
Seho Lim <limseho657424@gmail.com>
2017-01-12 10:06:57 -0600

    now map uses Integer

--- Day8/Solution.java
+++ Day8/Solution.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 class Solution{
     public static void main(String []argh){
-        Map<String, String> phoneBook = new HashMap<String, String>();
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
             String name = in.next();
-            String phone = in.next();
+            int phone = in.nextInt();
             phoneBook.put(name, phone);   
         }
         while(in.hasNext()){
